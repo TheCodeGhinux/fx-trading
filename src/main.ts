fae4a7ca4fff2b7b5380a52b0fe5757a9245be39
CodeGhinux <ilesanmibry@gmail.com>
2025-04-06 10:06:26 +0100

    feat: addded swagger documentation, Logger module and

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3002);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+
+  const logger = app.get(Logger);
+
+  app.enable('trust proxy');
+  app.enableCors();
+  app.setGlobalPrefix('api/v1', {
+    exclude: ['/', 'health', 'api', 'api/v1', 'api/docs', 'probe'],
+  });
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Fx Forage API Documentation')
+    .setDescription('Fx Forage API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+  const port = process.env.PORT ?? 4001;
+  await app.listen(port);
+  logger.log(`Application is running on port ${port}`);
+  logger.log({
+    message: 'Server started 🚀',
+    port,
+    url: `http://localhost:${port}/api/v1`,
+  });
 }
 bootstrap();
