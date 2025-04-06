5bfb290730155c5d0543abba4f7c19b5580308e2
CodeGhinux <ilesanmibry@gmail.com>
2025-04-06 22:17:52 +0100

    feat: addd typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dataSource from 'src/database/data-source';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [TypeOrmModule.forRootAsync({
+    useFactory: () => ({
+      ...dataSource.options,
+    }),
+    dataSourceFactory: async () => {
+      if (!dataSource.isInitialized) {
+        await dataSource.initialize();
+      }
+      return dataSource;
+    },
+  }), AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService, Logger],
 })
