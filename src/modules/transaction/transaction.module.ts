9717a3f336c264513be9f183bbf5e9fb75734f8c
CodeGhinux <ilesanmibry@gmail.com>
2025-04-07 22:40:12 +0100

    feat: create transaction implementation - added interfaces, repository, and updated entity for transactions - added service logic to create transaction

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transactions } from './entities/transaction.entity';
+import { EntitySchema } from 'typeorm';
+import { TransactionRepository } from './repositories/transaction.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Transactions, EntitySchema])
+  ],
   controllers: [TransactionController],
-  providers: [TransactionService],
+  providers: [TransactionService, TransactionRepository],
+  exports: [TransactionService]
 })
 export class TransactionModule {}
--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { TransactionRepository } from './repositories/transaction.repository';
+import { generateReferenceNumber } from 'src/common/helpers/reference-number.helper';
+import { CreateTransactionRecordOptions } from './interfaces/transaction.interface'
+import { User } from '../user/entities/user.entity';
+import { Wallet } from '../wallet/entities/wallet.entity';
+import { TransactionStatus, TransactionType } from './entities/transaction.entity';
+import { CustomHttpException } from 'src/common/custom.exception';
+import * as SYS_MSG from 'src/common/system-messages';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class TransactionService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    private readonly transactionRepository: TransactionRepository,
+    private readonly entityManager: EntityManager
+  ) {}
+
+  async create(user: User, payload: CreateTransactionDto, type: TransactionType, metadata: Record<string, any>) {
+    return this.createWithTransaction(user, payload, type, metadata);
   }
 
-  findAll() {
-    return `This action returns all transaction`;
-  }
+  async createWithTransaction(
+    user: User,
+    payload: CreateTransactionDto,
+    type: TransactionType,
+    metadata: Record<string, any>,
+    transactionManager?: EntityManager
+  ) {
+    const referenceNumber = generateReferenceNumber();
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
-  }
+    const createTransactionPayload: CreateTransactionRecordOptions = {
+      createPayload: {
+        user_id: user.id,
+        wallet_id: payload.wallet_id,
+        type,
+        amount: payload.amount,
+        currency: payload.currency,
+        status: TransactionStatus.COMPLETED,
+        reference: referenceNumber,
+        metadata,
+      },
+      transactionOptions: {
+        useTransaction: !!transactionManager,
+        transaction: transactionManager
+      }
+    };
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
+    console.log("Transaction entity value: ", !!transactionManager)
+    const transaction = await this.transactionRepository.create(createTransactionPayload);
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+    if (!transaction) {
+      throw new CustomHttpException('Failed to create transaction record', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return transaction;
   }
 }
